4174543d77bc243d4e9d18e94fdf0f5bd3f9eea8
Naive Roboticist <naiveroboticist@gmail.com>
2016-12-10 11:19:18 -0700

    This is all working w/LEDs.
    It's working both through the serial port connected via USB as well as
    the serial port connected via the Tx/Rx wires between the Pi and the
    Arduino. Very exciting.

--- scripts/commandTest.js
+++ scripts/commandTest.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
 
-const readline = require('readline');
-const RobotSerialInterface = require('robot-serial-iface').RobotSerialInterface;
+const readline = require('readline')
+const RobotSerialInterface = require('robot-serial-iface').RobotSerialInterface
 
-const DRIVE = 0x89;
+const DRIVE = 0x89
 // When hooked through the level converter, this is the
 // serial port tty
 const SERIAL_PORT = '/dev/ttyAMA0';
 // When hooked through the USB, this is the serial port
 // tty
-// const SERIAL_PORT = '/dev/ttyACM0';
-const SERIAL_OPTIONS = { baudrate: 9600 };
+// const SERIAL_PORT = '/dev/ttyACM0'
+const SERIAL_OPTIONS = { baudrate: 9600 }
 const SENSORS = [
   {
     name: 'temperature',
@@ -22,74 +22,72 @@ const SENSORS = [
     startByte: 0x02,
     numBytes: 2
   }
-];
+]
 
-var robot = new RobotSerialInterface();
+var robot = new RobotSerialInterface()
 
 // Start the interface...
-robot.start(SERIAL_PORT, SERIAL_OPTIONS, SENSORS);
+robot.start(SERIAL_PORT, SERIAL_OPTIONS, SENSORS)
 
-robot.on('temperature', function(temp) {
-  console.log("Temperature: %d", temp);
-});
+robot.on('temperature', function (temp) {
+  console.log('Temperature: %d', temp)
+})
 
-robot.on('humidity', function(hum) {
-  console.log("Humidity: %d", hum);
-});
+robot.on('humidity', function (hum) {
+  console.log('Humidity: %d', hum)
+})
 
-const uB = (word) => word >> 8;
+const uB = (word) => word >> 8
 
-const lB = (word) => word & 0x000000ff;
+const lB = (word) => word & 0x000000ff
 
 const sendCommand = (speed, angle) => {
-  robot.sendCommand(DRIVE, [uB(speed), lB(speed), uB(angle), lB(angle)]);
-};
+  robot.sendCommand(DRIVE, [uB(speed), lB(speed), uB(angle), lB(angle)])
+}
 
 // #################################################
 // Read commands from the command line from the user
 //
-const rl = readline.createInterface( {
+const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
-});
+})
 
-rl.setPrompt("Robot Command> ");
-rl.prompt();
+rl.setPrompt('Robot Command> ')
+rl.prompt()
 
 rl.on('line', (answer) => {
   if (answer === 'quit') {
-    more = false;
-    return rl.close();
+    return rl.close()
   }
 
   switch (answer) {
     case 'forward':
-      console.log("Drive forward");
-      sendCommand(70, 0);
-      break;
+      console.log('Drive forward')
+      sendCommand(70, 0)
+      break
     case 'backward':
-      console.log("Drive backward");
-      sendCommand(-70, 0);
-      break;
+      console.log('Drive backward')
+      sendCommand(-70, 0)
+      break
     case 'left':
-      console.log("Rotate left");
-      sendCommand(70, 1);
-      break;
+      console.log('Rotate left')
+      sendCommand(70, 1)
+      break
     case 'right':
-      console.log("Rotate right");
-      sendCommand(-70, 1);
-      break;
+      console.log('Rotate right')
+      sendCommand(-70, 1)
+      break
     case 'stop':
-      console.log("Stop");
-      sendCommand(0, 0);
-      break;
+      console.log('Stop')
+      sendCommand(0, 0)
+      break
     default:
-      console.log("Don't know what you're talking about: %j", answer);
+      console.log("Don't know what you're talking about: %j", answer)
   }
-  rl.prompt();
+  rl.prompt()
 }).on('close', () => {
-  console.log("Thanks. Bye");
-  process.exit();
-});
-
+  console.log('Thanks. Bye')
+  process.exit()
+})
 
--- src/sketch.ino
+++ src/sketch.ino
@@ -1,4 +1,7 @@
-#include "DHT.h"
+#include <Packet.h>
+// #include "DHT.h"
+
+#define SERIAL_BAUD_RATE 9600
 
 // Define the digital pins for the LEDs
 #define FORWARD 13
@@ -10,34 +13,36 @@
 #define DRIVE 0x89
 
 // Sensor output constants
-#define START_BYTE 0x13
+// #define START_BYTE 0x13
 
 #define COMMAND_PACKET_LENGTH 5
 
 // The sensor output looks like:
 // START_BYTE LEN_BYTE SENSOR_TYPE SENSOR_VALUE1 [SENSOR_VALUE2] [SENSOR_TYPE...]* CHECKSUM
 
+Packet inboundPkt;
+Packet outboundPkt;
 uint8_t commandPacket [COMMAND_PACKET_LENGTH];
 int currentPacketLength = 0;
 
-#define SENSOR_PACKET_LENGTH 6
-uint8_t sensorPacket [SENSOR_PACKET_LENGTH];
-#define ALT_SENSOR_PACKET_LENGTH 9
-uint8_t altSensorPacket [ALT_SENSOR_PACKET_LENGTH];
+//#define SENSOR_PACKET_LENGTH 6
+//uint8_t sensorPacket [SENSOR_PACKET_LENGTH];
+//#define ALT_SENSOR_PACKET_LENGTH 9
+//uint8_t altSensorPacket [ALT_SENSOR_PACKET_LENGTH];
 
 #define HUMIDITY_SENSOR 0x02
 #define TEMPERATURE_SENSOR 0x03
 
 // Sensor settings
-#define DHTPIN 4
-#define UNIT 0 // Fahrenheit
-#define DHTTYPE DHT11
+//#define DHTPIN 4
+//#define UNIT 0 // Fahrenheit
+//#define DHTTYPE DHT11
 
-DHT dht(DHTPIN, DHTTYPE);
+// DHT dht(DHTPIN, DHTTYPE);
 
 void setup()
 {
-  Serial.begin(57600);
+  Serial.begin(SERIAL_BAUD_RATE);
 
   // Set the digital pins to OUTPUT mode
   pinMode(FORWARD, OUTPUT);
@@ -45,14 +50,15 @@ void setup()
   pinMode(LEFT, OUTPUT);
   pinMode(RIGHT, OUTPUT);
 
-  dht.begin();
+  // dht.begin();
+  inboundPkt.reset();
 }
 
 void loop()
 {
 
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
+  float h = 40; // dht.readHumidity();
+  float t = 22; // dht.readTemperature();
 
   float tf = t * 1.8 + 32; // Convert fromn C to F
 
@@ -62,6 +68,16 @@ void loop()
 }
 
 void serialEvent() {
+  //if (Packet::read(Serial, inboundPkt)) {
+    //// If it returns true, we have a complete and valid packet ready to go.
+    //processCompletePacket();
+  //} else {
+    //// Well, just roll into the next serial event and gather
+    //// more data for the packet.
+    //// Skipping this for now
+    //// badPacket();
+  //}
+
   while (Serial.available()) {
     uint8_t b = Serial.read();
     if (currentPacketLength == 0) {
@@ -91,9 +107,38 @@ void serialEvent() {
   }
 }
 
+void badPacket() {
+  // Show that we got a bad command by
+  // turing all the LED's on.
+  ledsOff();
+
+  digitalWrite(FORWARD, true);
+  digitalWrite(BACKWARD, false);
+  digitalWrite(LEFT, false);
+  digitalWrite(RIGHT, true);
+}
+
+void badCommand() {
+  // Show that we got a bad command by
+  // turing all the LED's on.
+  ledsOff();
+
+  digitalWrite(FORWARD, true);
+  digitalWrite(BACKWARD, true);
+  digitalWrite(LEFT, true);
+  digitalWrite(RIGHT, true);
+}
+
 void processCompletePacket() {
-  int speed = commandPacket[1] << 8 | commandPacket[2];
-  int direction = commandPacket[3] << 8 | commandPacket[4];
+  uint8_t cmd = commandPacket[0]; // inboundPkt.nextValue(1); // What's the command?
+  if (cmd != DRIVE) {
+    // Invalid command
+    badCommand();
+    return;
+  }
+
+  int speed = (int) (commandPacket[1] << 8 | commandPacket[2]);
+  uint16_t direction = commandPacket[3] << 8 | commandPacket[4];
 
   ledsOff();
 
@@ -124,23 +169,34 @@ void ledsOff() {
 }
 
 void sendTempHumidValues(uint16_t humidity, uint16_t temperature) {
-  altSensorPacket[0] = START_BYTE;
-  altSensorPacket[1] = 0x06;
-  altSensorPacket[2] = HUMIDITY_SENSOR;
-  altSensorPacket[3] = humidity >> 8;
-  altSensorPacket[4] = humidity & 0x00ff;
-  altSensorPacket[5] = TEMPERATURE_SENSOR;
-  altSensorPacket[6] = temperature >> 8;
-  altSensorPacket[7] = temperature & 0x00ff;
+  outboundPkt.reset();
+
+  // altSensorPacket[0] = START_BYTE;
+  outboundPkt.append(0x06);
+  // altSensorPacket[1] = 0x06;
+  outboundPkt.append(HUMIDITY_SENSOR);
+  // altSensorPacket[2] = HUMIDITY_SENSOR;
+  outboundPkt.append(humidity >> 8);
+  // altSensorPacket[3] = humidity >> 8;
+  outboundPkt.append(humidity & 0x00ff);
+  // altSensorPacket[4] = humidity & 0x00ff;
+  outboundPkt.append(TEMPERATURE_SENSOR);
+  // altSensorPacket[5] = TEMPERATURE_SENSOR;
+  outboundPkt.append(temperature >> 8);
+  // altSensorPacket[6] = temperature >> 8;
+  outboundPkt.append(temperature & 0x00ff);
+  // altSensorPacket[7] = temperature & 0x00ff;
+  outboundPkt.complete(); // Deal with the checksum
 
   // Calculate the checksum for the packet
-  int total = 0;
-  for (int i=0; i<ALT_SENSOR_PACKET_LENGTH - 1; i++) {
-    total += altSensorPacket[i];
-  }
+  //int total = 0;
+  //for (int i=0; i<ALT_SENSOR_PACKET_LENGTH - 1; i++) {
+    //total += altSensorPacket[i];
+  //}
 
-  altSensorPacket[8] = -total; // Checksum
+  //altSensorPacket[8] = -total; // Checksum
 
-  Serial.write(altSensorPacket, ALT_SENSOR_PACKET_LENGTH);
+  outboundPkt.write(Serial);
+  // Serial.write(altSensorPacket, ALT_SENSOR_PACKET_LENGTH);
 }
 
