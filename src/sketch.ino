e81d25dfaa916a7055802b8893f780c1ac124cba
Dave <dj0hnve@gmail.com>
2016-12-11 15:01:38 -0700

    Added conditional compilation for STUB_MINION.

--- src/sketch.ino
+++ src/sketch.ino
@@ -2,13 +2,17 @@
 #include <CommandPacket.h>
 // #include "DHT.h"
 
+#define STUB_MINION true
+
 #define SERIAL_BAUD_RATE 9600
 
+#ifdef STUB_MINION
 // Define the digital pins for the LEDs
 #define FORWARD 13
 #define BACKWARD 12
 #define LEFT 11
 #define RIGHT 10
+#endif
 
 // The incoming commands
 #define DRIVE 0x89
@@ -16,7 +20,7 @@
 // Sensor output constants
 // #define START_BYTE 0x13
 
-#define COMMAND_PACKET_LENGTH 5
+// #define COMMAND_PACKET_LENGTH 5
 
 // The sensor output looks like:
 // START_BYTE LEN_BYTE SENSOR_TYPE SENSOR_VALUE1 [SENSOR_VALUE2] [SENSOR_TYPE...]* CHECKSUM
@@ -24,15 +28,17 @@
 CommandPacket inboundPkt(DRIVE, 5);
 Packet outboundPkt;
 // uint8_t commandPacket [COMMAND_PACKET_LENGTH];
-int currentPacketLength = 0;
+// int currentPacketLength = 0;
 
 //#define SENSOR_PACKET_LENGTH 6
 //uint8_t sensorPacket [SENSOR_PACKET_LENGTH];
 //#define ALT_SENSOR_PACKET_LENGTH 9
 //uint8_t altSensorPacket [ALT_SENSOR_PACKET_LENGTH];
 
+#ifdef STUB_MINION
 #define HUMIDITY_SENSOR 0x02
 #define TEMPERATURE_SENSOR 0x03
+#endif
 
 // Sensor settings
 //#define DHTPIN 4
@@ -46,10 +52,12 @@ void setup()
   Serial.begin(SERIAL_BAUD_RATE);
 
   // Set the digital pins to OUTPUT mode
+#ifdef STUB_MINION
   pinMode(FORWARD, OUTPUT);
   pinMode(BACKWARD, OUTPUT);
   pinMode(LEFT, OUTPUT);
   pinMode(RIGHT, OUTPUT);
+#endif
 
   // dht.begin();
   inboundPkt.reset();
@@ -58,6 +66,7 @@ void setup()
 void loop()
 {
 
+#ifdef STUB_MINION
   float h = 40; // dht.readHumidity();
   float t = 22; // dht.readTemperature();
 
@@ -66,6 +75,7 @@ void loop()
   sendTempHumidValues(h, tf);
 
   delay(2000);
+#endif
 }
 
 void serialEvent() {
@@ -74,6 +84,7 @@ void serialEvent() {
   } else {
     // This was a bad or incomplete packet. Moving on.
     inboundPkt.reset();
+    badPacket();
   }
   //if (Packet::read(Serial, inboundPkt)) {
     //// If it returns true, we have a complete and valid packet ready to go.
@@ -125,16 +136,16 @@ void badPacket() {
   digitalWrite(RIGHT, true);
 }
 
-void badCommand() {
-  // Show that we got a bad command by
-  // turing all the LED's on.
-  ledsOff();
+//void badCommand() {
+  //// Show that we got a bad command by
+  //// turing all the LED's on.
+  //ledsOff();
 
-  digitalWrite(FORWARD, true);
-  digitalWrite(BACKWARD, true);
-  digitalWrite(LEFT, true);
-  digitalWrite(RIGHT, true);
-}
+  //digitalWrite(FORWARD, true);
+  //digitalWrite(BACKWARD, true);
+  //digitalWrite(LEFT, true);
+  //digitalWrite(RIGHT, true);
+//}
 
 void processCompletePacket() {
   //uint8_t cmd = commandPacket[0]; // inboundPkt.nextValue(1); // What's the command?
@@ -147,6 +158,7 @@ void processCompletePacket() {
   int speed = (int) inboundPkt.nextValue(2); // (commandPacket[1] << 8 | commandPacket[2]);
   uint16_t direction = inboundPkt.nextValue(2); // commandPacket[3] << 8 | commandPacket[4];
 
+#ifdef STUB_MINION
   ledsOff();
 
   if (speed > 0) {
@@ -165,16 +177,20 @@ void processCompletePacket() {
   } else {
     ledsOff(); // Same as stop
   }
+#endif
 
 }
 
+#ifdef STUB_MINION
 void ledsOff() {
   digitalWrite(FORWARD, false);
   digitalWrite(BACKWARD, false);
   digitalWrite(LEFT, false);
   digitalWrite(RIGHT, false);
 }
+#endif
 
+#ifdef STUB_MINION
 void sendTempHumidValues(uint16_t humidity, uint16_t temperature) {
   outboundPkt.reset();
 
@@ -206,4 +222,5 @@ void sendTempHumidValues(uint16_t humidity, uint16_t temperature) {
   outboundPkt.write(Serial);
   // Serial.write(altSensorPacket, ALT_SENSOR_PACKET_LENGTH);
 }
+#endif
 
